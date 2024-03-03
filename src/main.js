b1d521630eaafd6e6b77b1f1c6ec769904575b12
minoliAlles <141716046+minoliAlles@users.noreply.github.com>
2024-03-04 01:59:11 +0530

    vuetify issue fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
-import App from './App.vue';
-import vuetify from './plugins/vuetify';
+import App from '@/App.vue'; // Ensure this points to the location of App.vue
+import vuetify from '@/plugins/vuetify'; // Ensure this points to the location of vuetify.js
 
 Vue.config.productionTip = false;
 
