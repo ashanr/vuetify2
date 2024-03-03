291982f74cc4d95842ea63ffd508592851c97dd1
minoliAlles <141716046+minoliAlles@users.noreply.github.com>
2024-03-04 01:00:14 +0530

    initial commit

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app>
+      <v-main>
+        <v-container>Hello World</v-container>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+  };
+  </script>
+  
+  <style>
+  /* Add your styles here */
+  </style>
+  
